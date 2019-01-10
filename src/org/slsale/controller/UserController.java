efc029d24bb0cca24462b4c384dd50263feca30d
oyangfong <oyangfong@yeah.net>
2019-01-10 14:06:12 +0800

    add 权限管理

--- src/org/slsale/controller/UserController.java
+++ src/org/slsale/controller/UserController.java
@@ -1,8 +1,13 @@
 package org.slsale.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+	@RequestMapping("user/list")
+   public String list() {
+	   return "user/userlist";
+   }
    
 }
